6d98ac6551fe96849a88ef733208264457f15751
MikiBersa <trombettamiki@gmail.com>
2024-01-27 15:34:42 +0100

    Added SendEmail in Subscribed

--- CKBApplicationServer/src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/tournament/TournamentService.java
+++ CKBApplicationServer/src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/tournament/TournamentService.java
@@ -4,6 +4,10 @@ import BersaniChiappiniFraschini.CKBApplicationServer.notification.NotificationS
 import BersaniChiappiniFraschini.CKBApplicationServer.user.AccountType;
 import BersaniChiappiniFraschini.CKBApplicationServer.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,6 +15,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Service that manages tournaments (will become big)
@@ -21,6 +28,10 @@ public class TournamentService {
     private final TournamentRepository tournamentRepository;
     private final UserDetailsService userDetailsService;
     private final NotificationService notificationService;
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(5);
+
+    private final MongoTemplate mongoTemplate;
 
     public ResponseEntity<PostResponse> createTournament(TournamentCreationRequest request){
 
@@ -58,10 +69,52 @@ public class TournamentService {
         tournamentRepository.insert(tournament);
 
         // Notify the whole world about this
-        // TODO Run this in a different thread
-        notificationService.sendTournamentCreationNotifications(tournament);
+        Runnable taskSendEmail = () -> {
+            notificationService.sendTournamentCreationNotifications(tournament);
+        };
 
+        executor.submit(taskSendEmail);
         // for each user in request.invited_managers, send invite request
+
+        return ResponseEntity.ok(null);
+    }
+
+    public ResponseEntity<PostResponse> subscribeTournament(TournamentSubscribeRequest request){
+
+        var context = SecurityContextHolder.getContext();
+        var auth = context.getAuthentication();
+
+        AccountType accountType = AccountType.valueOf(auth.getAuthorities().stream().toList().get(0).toString());
+        if(accountType != AccountType.STUDENT){
+            var res = new PostResponse("Cannot subscribe to tournament as educator");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+        }
+
+        String username = auth.getName();
+        var title = request.getTitle();
+
+        // I check if the user is already subscribed to the tournament
+        Optional<Tournament> tournament = tournamentRepository.findBySubscribed_user(username, title);
+
+        if(tournament.isPresent()){
+            var res = new PostResponse("Already subscribed in this tournament");
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+        }
+
+        //Add the user to the subscribed field of the tournament
+        User user = (User) userDetailsService.loadUserByUsername(username);
+
+        var update = new Update();
+        update.push("subscribed_users", user);
+        var criteria = Criteria.where("title").in(title);
+        mongoTemplate.updateFirst(Query.query(criteria), update, "tournament");
+
+        //send e-mail of the subscription
+        Runnable taskSendEmail = () -> {
+            notificationService.sendNotification(user.getEmail(), "You have successfully registered for the " + "'%s'".formatted(title) + " tournament");
+        };
+
+        executor.submit(taskSendEmail);
 
         return ResponseEntity.ok(null);
     }
