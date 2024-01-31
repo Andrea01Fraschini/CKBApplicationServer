28a1832cf7448a1e407e15e33f8303fe2b4c6adb
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-01-31 18:41:23 +0100

    IT WORKS

---
+++ src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/test_groupRepository.java
@@ -0,0 +1,7 @@
+package BersaniChiappiniFraschini.CKBApplicationServer;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface test_groupRepository extends MongoRepository<test_group, String> {
+
+}
