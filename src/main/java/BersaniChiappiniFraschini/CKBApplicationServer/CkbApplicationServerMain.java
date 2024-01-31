e9304ae85115fc49d96c788f7524007ada4efe5e
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-01-31 11:45:49 +0100

    Refactored everything

--- src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/CkbApplicationServerMain.java
+++ src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/CkbApplicationServerMain.java
@@ -2,9 +2,16 @@ package BersaniChiappiniFraschini.CKBApplicationServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CkbApplicationServerMain {
+public class CkbApplicationServerMain extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CkbApplicationServerMain.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CkbApplicationServerMain.class, args);
