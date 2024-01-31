f30b0cfc0060399b1c06037dc64a0da3e0d96450
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-01-31 11:56:51 +0100

    Refactored everything

--- src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/authentication/AuthenticationController.java
+++ src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/authentication/AuthenticationController.java
@@ -1,10 +1,7 @@
 package BersaniChiappiniFraschini.CKBApplicationServer.authentication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,5 +21,10 @@ public class AuthenticationController {
             @RequestBody LoginRequest request
     ){
         return authService.login(request);
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello there!";
     }
 }
