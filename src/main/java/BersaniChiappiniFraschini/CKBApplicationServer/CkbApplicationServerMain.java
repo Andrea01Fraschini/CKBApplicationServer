d90a8ab26e05f1d150f38fc5206e452a33885646
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-01-31 15:12:38 +0100

    Rebuilt app

--- src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/CkbApplicationServerMain.java
+++ src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/CkbApplicationServerMain.java
@@ -3,17 +3,12 @@ package BersaniChiappiniFraschini.CKBApplicationServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class CkbApplicationServerMain extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CkbApplicationServerMain.class);
-	}
+@SpringBootApplication
+public class CkbApplicationServerMain{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CkbApplicationServerMain.class, args);
