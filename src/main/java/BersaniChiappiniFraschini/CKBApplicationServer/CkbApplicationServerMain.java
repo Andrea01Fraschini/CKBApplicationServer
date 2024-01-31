896850e71cb062e4e2342d8987e4f7dce602cb54
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-01-31 14:18:07 +0100

    Changed database location to atlas

--- src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/CkbApplicationServerMain.java
+++ src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/CkbApplicationServerMain.java
@@ -1,7 +1,9 @@
 package BersaniChiappiniFraschini.CKBApplicationServer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
