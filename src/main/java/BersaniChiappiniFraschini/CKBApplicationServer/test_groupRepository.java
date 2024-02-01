177a1ce75e71b4f3b6a08ca1cd0ab15d26b11115
Andrea Fraschini <andrea1.fraschini@mail.polimi.it>
2024-02-01 17:22:51 +0100

    intellij merda

--- src/main/java/BersaniChiappiniFraschini/CKBApplicationServer/test_groupRepository.java
+++
@@ -1,7 +0,0 @@
-package BersaniChiappiniFraschini.CKBApplicationServer;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface test_groupRepository extends MongoRepository<test_group, String> {
-
-}
